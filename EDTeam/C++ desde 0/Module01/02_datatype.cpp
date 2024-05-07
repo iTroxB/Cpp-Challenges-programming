c0ca7e2fa810ea9d3f6ce2cef802a2e80445b6e0
iTrox <143133090+iTroxB@users.noreply.github.com>
2024-05-07 18:07:54 -0400

    Create 02_datatype.cpp

---
+++ EDTeam/C++ desde 0/Module01/02_datatype.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello Javier! \n";
+    std::cout << 5.0f / 2 << "\n";
+    std::cout << true << "\n";
+    std::cout << !true << "\n";
+    std::cout << (true && false) << "\n";
+    std::cout << 'a' << "\n";
+
+
+    return 0;
+}
