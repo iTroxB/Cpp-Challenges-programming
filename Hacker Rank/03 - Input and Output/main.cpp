5e7c7061210fc9cf7ad71daa824fdc10439fc12b
iTrox <143133090+iTroxB@users.noreply.github.com>
2024-05-07 01:07:40 -0400

    Update main.cpp

--- Hacker Rank/03 - Input and Output/main.cpp
+++ Hacker Rank/03 - Input and Output/main.cpp
@@ -1,7 +1,24 @@
+#include <cmath>
 #include <iostream>
 using namespace std;
 
 int main() {
+    int x;
+    int y;
+    int z;
+    
+    while (true) {
+        //cout << "Enter 3 numbers into 0 to 1000: ";
+        cin >> x >> y >> z;
+        
+        if ((1 <= x && x <= 1000) && (1 <= y && y <= 1000) && (1 <= y && y <= 1000)) {
+            int i = x + y + z;
+            cout << i;
+            break;
+        } else {
+            cout << "Enter only numbers into 0 to 1000!";
+        }
+    }
 
-  return 0;
+    return 0;
 }
