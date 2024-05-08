3c4f6e076b335fe205ac7e178f483f6f6cde61ed
iTrox <143133090+iTroxB@users.noreply.github.com>
2024-05-08 17:43:01 -0400

    Create 04_namespaces.cpp

---
+++ EDTeam/C++ desde 0/Module04/04_namespaces.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+namespace Math {
+    int sum(int a, int b) {
+
+        return a + b;
+    }
+
+    int abs(int x) {
+
+        if (x < 0) {
+            return -x;
+        } else {
+            return x;
+        }
+    }
+}
+
+int main() {
+
+    std::cout << "5 + 3 = " << Math::sum(5, 3) << "\n";
+    std::cout << "|-5| = " << Math::abs(-5) << "\n";
+    return 0;
+}
